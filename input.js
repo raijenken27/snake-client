87a08059a0c8c84261c0f499b1772551c6d485fc
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-30 13:23:47 -0600

    Adding the WASD movement

--- input.js
+++ input.js
@@ -1,18 +1,28 @@
 // input.js
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+let connection;
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => {
+    if (key === "w") {
+      connection.write("Move: up");
+    } else if (key === "a") {
+      connection.write("Move: left");
+    } else if (key === "s") {
+      connection.write("Move: down");
+    } else if (key === "d") {
+      connection.write("Move: right");
+    } else if (key === '\u0003') {
+      process.exit(); // Ctrl + C exits the game
+    }
+  });
 
   return stdin;
 };
