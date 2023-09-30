274156a42e71526f33599a3755d6dff659c834c6
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-30 11:59:11 -0600

    Adding client.js

---
+++ client.js
@@ -0,0 +1,9 @@
+// client.js
+const net = require("net");
+
+// Function to connect to the server
+const connect = function () {
+  // ... (existing connect function code)
+};
+
+module.exports = { connect }; // Export the connect function
